94410c8ba54d3c20abbe1f1cd59d6238b4889e45
johnrds <121064478+johnrds@users.noreply.github.com>
2023-01-24 11:16:37 -0300

    Created a label to chose the amount of grids

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 const gridPanel = document.querySelector('.gridPanel');
 
-for(let i = 0; i < (16*16); i++){
-    let grids = document.createElement('div');
-    grids.classList = `grid ${i}`;
-    gridPanel.appendChild(grids);
+function createGrid(n) {
+
+    const prevGrids = document.querySelectorAll('.grid');
+    prevGrids.forEach(grid => { grid.remove() });
+
+    for (let i = 0; i < (n * n); i++) {
+        let grid = document.createElement('div');
+        grid.classList = `grid ${i + 1}`;
+        grid.addEventListener('mouseover', () => {
+            grid.classList.add('hover');
+        });
+        gridPanel.appendChild(grid);
+    };
+    gridPanel.style.gridTemplateColumns = `repeat(${n}, 1fr)`;
+    gridPanel.style.gridTemplateRows = `repeat(${n}, 1fr)`;
 };
 
-const allGrids = document.querySelectorAll('.grid')
-allGrids.forEach(grid => {
-    grid.addEventListener('mouseover', ()=>{
-        grid.classList.add('hover');
-    });
+const gridLabel = document.querySelector('#grid');
+gridLabel.addEventListener('keypress', (e) => {
+    if (e.key !== 'Enter'
+        || isNaN(gridLabel.value)
+        || gridLabel.value === '0'
+        || gridLabel.value === ''
+        || gridLabel.value > 50) return;
+
+    createGrid(gridLabel.value);
+    gridLabel.value = "";
 });
