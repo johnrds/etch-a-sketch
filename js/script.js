d138eb2a2085e27f683f1e3afe2c9331f8ba75f4
johnrds <121064478+johnrds@users.noreply.github.com>
2023-02-04 12:51:09 -0300

    Updated styling/added bgColor Picker

--- js/script.js
+++ js/script.js
@@ -5,11 +5,12 @@ function createGrid(n) {
 
     for (let i = 0; i < (n * n); i++) {
         let grid = document.createElement('div');
-        grid.classList = `grid ${i + 1}`;
-        grid.style.border = '1px solid var(--clr-gray-200)';
+        grid.classList = `grid ${i + 1} background`;
+        grid.style.border = '1px solid hsl(0, 0%, 90%)';
+        grid.style.backgroundColor = bgClr;
         grid.addEventListener('mouseover', () => {
+            grid.classList.remove('background');
             grid.style.backgroundColor = currentClr;
-
         });
         gridPanel.appendChild(grid);
     };
@@ -17,26 +18,30 @@ function createGrid(n) {
     gridPanel.style.gridTemplateRows = `repeat(${n}, 1fr)`;
 };
 
-const gridLabel = document.querySelector('#grid');
-gridLabel.addEventListener('keypress', (e) => {
-    if (e.key !== 'Enter'
-        || isNaN(gridLabel.value)
-        || gridLabel.value === '0'
-        || gridLabel.value === ''
-        || gridLabel.value > 50) return;
 
-    createGrid(gridLabel.value);
+const gridRange = document.querySelector('#gridRange');
+const gridLabel = document.querySelector('.rangeValue');
+gridRange.addEventListener('change', (e) => {
+    resetValues();
+    createGrid(e.target.value);
 });
 
-const resetBtn = document.querySelector('.resetBtn');
-resetBtn.addEventListener('click', () => {
-    createGrid(Math.sqrt(gridPanel.lastChild.classList[1]));
-});
+gridRange.addEventListener('mousemove', (e) => {
+    gridLabel.textContent = `${e.target.value} x ${e.target.value}`
+})
 
 let currentClr = '#000000'
-const colorPicker = document.querySelector('#color');
-colorPicker.addEventListener('change', (e) => {
+const brushClrPicker = document.querySelector('#brColor');
+brushClrPicker.addEventListener('change', (e) => {
     currentClr = e.target.value;
+});
+
+let bgClr = '#FFFFFF'
+const bgClrPicker = document.querySelector('#bgColor');
+bgClrPicker.addEventListener('change', (e) => {
+    bgClr = e.target.value;
+    const prevGrids = document.querySelectorAll('.grid.background');
+    prevGrids.forEach(grid => { grid.style.backgroundColor = bgClr });
 });
 
 const borderBtn = document.querySelector('.gridBorder');
@@ -44,9 +49,22 @@ borderBtn.addEventListener('click', () => {
     const grids = document.querySelectorAll('.grid');
     grids.forEach(grid => {
         if (grid.style.border === '') {
-            grid.style.border = '1px solid var(--clr-gray-200)';
+            grid.style.border = '1px solid hsl(0, 0%, 90%)';
         } else { grid.style.border = '' };
     });
 });
+
+const resetBtn = document.querySelector('.resetBtn');
+resetBtn.addEventListener('click', () => {
+    resetValues();
+    createGrid(Math.sqrt(gridPanel.lastChild.classList[1]));
+});
+
+function resetValues() {
+    currentClr = '#000000';
+    brushClrPicker.value = '#000000';
+    bgClr = '#FFFFFF';
+    bgClrPicker.value = '#FFFFFF';
+};
 
 createGrid(16);
